4f02299eb482471d1f2a06a36af9381537648c0a
Oyshik <moyshik7@gmail.com>
2023-07-31 23:11:25 +0600

    Adding Google script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+		<div>
+      		<Script src="https://www.googletagmanager.com/gtag/js?id=G-BZ71381ERP" />
+      		<Script id="google-analytics">
+        		{`
+          			window.dataLayer = window.dataLayer || [];
+          			function gtag(){dataLayer.push(arguments);}
+          			gtag('js', new Date());
+ 
+          			gtag('config', 'G-BZ71381ERP');
+        		`}
+      		</Script>
+			<Component {...pageProps} />
+    	</div>
+    )
 }
--- pages/rick.tsx
+++ pages/rick.tsx
@@ -7,26 +7,7 @@ const HomePage = () => {
     return (
     <div className={styles.main}>
         <Head>
-            <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
-            <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
-            <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
-            <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
-            <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
-            <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
-            <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
-            <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-            <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-            <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-            <link rel="manifest" href="/manifest.json"/>
-            <meta name="msapplication-TileColor" content="#ffffff"/>
-            <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
-            <meta name="theme-color" content="#ffffff"/>
-            
-            <title>Sandhani - Blood Donation Organization</title>
-            <link rel="icon" href="/favicon.ico" />
+            <title>You Have Been RickRolled</title>
             <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         </Head>
 
@@ -37,10 +18,6 @@ const HomePage = () => {
         <main className={styles.content}>
             <div className={`${styles.item} ${styles.bg_orange}`}>
                 <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1&mute=0&loop=1" title="Never Gonna Give You Up" frameBorder="0" allow="autoplay" allowFullScreen ></iframe>
-            </div>
-            
-            <div className={`${styles.item} ${styles.bg_orange}`}>
-                <iframe src="https://www.youtube.com/embed/Tb6xGDiSscc?controls=0&autoplay=1&mute=0&loop=1" title="Never Gonna Give You Up" frameBorder="0" allow="autoplay" allowFullScreen ></iframe>
             </div>
         </main>
     </div>);
