2eac0d22244e46e4f354ceb477402a62ed28d99b
Oyshik <moyshik7@gmail.com>
2023-08-10 03:32:40 +0600

    fuck

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
         <Html lang="en">
         <body>
             <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
             <Main />
             <NextScript />
