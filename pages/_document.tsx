656ef22ff4a65460da9cb8d99f970509a8897cc5
Oyshik <moyshik7@gmail.com>
2023-08-10 03:23:53 +0600

    No need for google material icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
-            <Head>
-				<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons?display=swap" media="all"/>
-            </Head>
         <body>
+            <Head />
             <Main />
             <NextScript />
         </body>
