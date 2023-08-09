6c9508657d121a0b08d0fa3531ed25f25352028f
Oyshik <moyshik7@gmail.com>
2023-08-10 03:34:53 +0600

    fix 01

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,6 @@ export default function Document() {
     return (
         <Html lang="en">
         <body>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            </Head>
             <Main />
             <NextScript />
         </body>
