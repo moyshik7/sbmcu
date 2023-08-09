9e17e5feb4131da1c61788e64f020f70d1b4afc9
Oyshik <moyshik7@gmail.com>
2023-08-10 03:30:37 +0600

    wut

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,8 @@ export default function Document() {
     return (
         <Html lang="en">
         <body>
-            <Head />
+            <Head>
+            </Head>
             <Main />
             <NextScript />
         </body>
