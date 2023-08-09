d030eda5c0943c5527569a9e04f46ab0df55b380
Oyshik <moyshik7@gmail.com>
2023-08-10 03:23:08 +0600

    changed some styles

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import styles from "@/styles/navbar.module.css"
 import Head from "next/head";
+import Script from "next/script";
 
 const NavItem = ({ text, href, active }) => {
   return (
@@ -21,40 +22,34 @@ const MENU_LIST = [
   { text: "Contact", href: "/contact/" },
 ];
 const Navbar = () => {
-  const [navActive, setNavActive] = useState(null);
-  const [activeIdx, setActiveIdx] = useState(-1);
+    const [navActive, setNavActive] = useState(null);
+    const [activeIdx, setActiveIdx] = useState(-1);
 
-  return (<div>
-    <Head>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons?display=swap" rel="stylesheet"></link>
-    </Head>
-    <header>
-      <div className={styles.nav}>
-        <Link href={"/"}>
-            <h1 className={styles.logo}>Sandhani</h1>
-        </Link>
-        <div
-          onClick={() => setNavActive(!navActive)}
-          className={styles.nav__menu_bar}
-        >
-          <i className="material-icons">menu</i>
-        </div>
-        <div className={`${navActive ? styles.active : ""} ${styles.nav__menu_list}`}>
-          {MENU_LIST.map((menu, idx) => (
-            <div
-              onClick={() => {
-                setActiveIdx(idx);
-                setNavActive(false);
-              }}
-              key={menu.text}
-            >
-              <NavItem active={activeIdx === idx} {...menu} />
-            </div>
-          ))}
-        </div>
-      </div>
+    return (<div>
+        <Script src="https://kit.fontawesome.com/e8cf12cfb9.js" crossorigin="anonymous" />
+        <header>
+            <div className={styles.nav}>
+                <Link href={"/"}>
+                    <h1 className={styles.logo}>Sandhani</h1>
+        		</Link>
+        		<div onClick={() => setNavActive(!navActive)} className={styles.nav__menu_bar}>
+					{ navActive ? <i className='fas fa-times' /> : <i className='fas fa-bars' /> }
+				</div>
+        		<div className={`${navActive ? styles.active : ""} ${styles.nav__menu_list}`}>
+          			{MENU_LIST.map((menu, idx) => (
+            			<div
+              				onClick={() => {
+                				setActiveIdx(idx);
+                				setNavActive(false);
+              				}}
+              				key={menu.text}
+            			>
+              				<NavItem active={activeIdx === idx} {...menu} />
+            			</div>
+          			))}
+        		</div>
+      	</div>
     </header>
-  </div>);
-};
+</div>)};
 
 export default Navbar;
