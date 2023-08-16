f6d0c967bc981922636cf12170b9e4e6cb34f66c
Oyshik <moyshik7@gmail.com>
2023-08-17 00:05:52 +0600

    Fixing Adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (<>
-        <Script 
-            async={true}
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9901731849909509"
-            crossOrigin="anonymous"
-            onError={ (e) => { console.error('Script failed to load', e) }}
-        />
         <Component {...pageProps} />
     </>)
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
     return (
     <Html lang="en">
         <Head>
+            <Script 
+                async={true}
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9901731849909509"
+                crossOrigin="anonymous"
+                onError={ (e) => { console.error('Script failed to load', e) }}
+            />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"anonymous"} />
         </Head>
